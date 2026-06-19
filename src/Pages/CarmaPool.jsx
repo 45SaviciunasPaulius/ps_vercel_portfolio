6bda32b7d82db7359cadc0060fa7d9d3869eb348
Paulius Savičiūnas <psaviciunas@gmail.com>
2026-06-19 22:28:48 +0300

    add detailed info on projects 4

--- src/Pages/CarmaPool.jsx
+++ src/Pages/CarmaPool.jsx
@@ -1,7 +1,40 @@
+import localization from "../localization.json";
+import { langContext } from "../App";
+import { useContext } from "react";
+
 export default function CarmaPool() {
+  const language = useContext(langContext);
+  const projects =
+    localization[language.lang].projects.project_4_detailed ??
+    localization.LT.projects.project_4_detailed;
+
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-heading text-4xl">Carma Pool</h1>
+    <div className="p-20">
+      <div>
+        <h1>{projects.title}</h1>
+        <div>{projects.date}</div>
+        <div>{projects.grade}</div>
+      </div>
+
+      <div>
+        <div>{projects.about}</div>
+        <ol className="list-decimal">
+          {Object.values(projects.functions).map((element, i) => (
+            <li key={i}>{element}</li>
+          ))}
+        </ol>
+      </div>
+
+      <div>
+        <h2>{projects.tech}</h2>
+        <div></div>
+      </div>
+
+      <a href="https://carmapool.saviciunas.lt" target="_blank">
+        {projects.button}
+      </a>
+
+      <div>Photos</div>
     </div>
   );
 }
